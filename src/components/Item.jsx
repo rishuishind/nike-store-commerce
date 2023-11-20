05b0eec5fe83cc2017c17e8a7519510602a240e3
rishabh <hespnod2348@gmai.com>
2023-11-20 23:54:42 +0530

    made the sales component layout fully

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,12 +1,11 @@
 /* eslint-disable react/prop-types */
 import { StarIcon, ShoppingBagIcon } from '@heroicons/react/24/solid';
 
-const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
-    console.log('hiii');
+const Item = ({ ifExists, id, color, shadow, title, text, img, btn, rating, price }) => {
     return (
         <>
-            <div className={`relative bg-gradient-to-b ${color} ${shadow} grid items-center justify-items-center rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full hover:scale-105`}>
-                <div className='grid items-center justify-items-center'>
+            <div className={`relative bg-gradient-to-b ${color} ${shadow} grid items-center ${ifExists ? 'justify-items-start' : 'justify-items-center'} rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full hover:scale-105`}>
+                <div className={` grid items-center ${ifExists ? 'justify-items-start' : 'justify-items-center'}`}>
                     <h1 className='text-slate-200 text-xl lg:text-lg md:text-base font-medium filter drop-shadow'>{title}</h1>
                     <p className='text-slate-200 text-base md:text-sm font-normal filter drop-shadow'>{text}</p>
 
@@ -22,8 +21,8 @@ const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
                         <button type='button' className='bg-white/90 blur-effect-theme button-theme p-0.5 shadow shadow-sky-200 text-sm px-2 py-1'>{btn}</button>
                     </div>
                 </div>
-                <div className='flex items-center'>
-                    <img src={img} alt="img/item-img" className='h-36 w-64 transitions-theme hover:-rotate-12' />
+                <div className={`flex items-center ${ifExists ? 'absolute top-5 right-1' : 'justify-center'}`}>
+                    <img src={img} alt={`img/item-img/${id}`} className={` transitions-theme hover:-rotate-12 ${ifExists ? 'h-auto w-64 lg:w-56 md:w-48 -rotate-[35deg]' : 'h-36 w-64'}`} />
                 </div>
             </div>
         </>
