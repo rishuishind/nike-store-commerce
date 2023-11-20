851b4a6e2dd685d37d48646563713c37138bcc1e
rishabh <hespnod2348@gmai.com>
2023-11-20 20:58:10 +0530

    made layout for top rated sales with css

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,21 +5,25 @@ const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
     console.log('hiii');
     return (
         <>
-            <div>
-                <div>
-                    <h1>{title}</h1>
-                    <p>{text}</p>
-                    <div>
-                        <div><h1>{price}</h1></div>
-                        <div><StarIcon className='icon-style text-slate-900' /><h1>{rating}</h1></div>
+            <div className={`relative bg-gradient-to-b ${color} ${shadow} grid items-center justify-items-center rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full hover:scale-105`}>
+                <div className='grid items-center justify-items-center'>
+                    <h1 className='text-slate-200 text-xl lg:text-lg md:text-base font-medium filter drop-shadow'>{title}</h1>
+                    <p className='text-slate-200 text-base md:text-sm font-normal filter drop-shadow'>{text}</p>
+
+
+                    <div className='flex items-center justify-between w-28 my-2'>
+                        <div className='flex items-center bg-white/80 px-1 rounded blur-effect-theme'><h1 className='text-sm font-medium '>Rs {price}</h1></div>
+                        <div className='flex items-center gap-1'><StarIcon className='icon-style w-5 h-5 md:w-4 md:h-4' /><h1 className='md:text-sm font-normal text-slate-100'>{rating}</h1></div>
                     </div>
-                    <div>
-                        <button type='button' className=''><ShoppingBagIcon className='icon-style text-slate-900' /></button>
-                        <button type='button'>{btn}</button>
+
+
+                    <div className='flex items-center gap-3'>
+                        <button type='button' className='bg-white/90 blur-effect-theme button-theme p-0.5 shadow shadow-sky-200'><ShoppingBagIcon className='icon-style text-slate-900' /></button>
+                        <button type='button' className='bg-white/90 blur-effect-theme button-theme p-0.5 shadow shadow-sky-200 text-sm px-2 py-1'>{btn}</button>
                     </div>
                 </div>
-                <div>
-                    <img src={img} alt="img/item-img" className='' />
+                <div className='flex items-center'>
+                    <img src={img} alt="img/item-img" className='h-36 w-64 transitions-theme hover:-rotate-12' />
                 </div>
             </div>
         </>
